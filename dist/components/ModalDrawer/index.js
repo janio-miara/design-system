e3ef2cc8be2fb4c64377d0c6f43596b127006964
sergio-dev-pro-max <sergiolucasoliveira02@gmail.com>
2022-02-01 00:54:54 -0300

    chore: build files

--- dist/components/ModalDrawer/index.js
+++ dist/components/ModalDrawer/index.js
@@ -1,27 +1,41 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
 import { Button } from '../Button';
 import { Text } from '../Text';
 import * as Style from './styles';
+import { animationCloseSideModal } from '../../utils/animationCloseSideModal';
 export const ModalDrawer = props => {
-    const { open, close, title, subTitle, icon, children, action, notHeader } = props;
+    const { open, close, title, subTitle, icon, children, action, notHeader, side } = props;
     const ref = useRef();
-    useOnClickOutside(ref, close);
+    const closeModal = () => __awaiter(void 0, void 0, void 0, function* () {
+        yield animationCloseSideModal(ref.current, side);
+        if (close)
+            close();
+    });
+    useOnClickOutside(ref, closeModal);
     return open
         ? ReactDOM.createPortal(React.createElement(Style.Container, Object.assign({}, props),
-            React.createElement("div", { ref: ref },
-                React.createElement(Style.ContainerFilter, Object.assign({}, props),
-                    !notHeader ? (React.createElement(React.Fragment, null,
-                        React.createElement("div", { className: "wrapper-heading" },
-                            icon && React.createElement("span", { className: "icon" }, icon),
-                            React.createElement("div", null,
-                                React.createElement(Text, { element: "h3", bold: true, color: "white" }, title),
-                                React.createElement(Text, { element: "span", size: "p3", color: "white" }, subTitle && subTitle))),
-                        React.createElement("div", { className: "content" }, children))) : (React.createElement("div", { className: "content-not-header" }, children)),
-                    React.createElement("div", { className: "button-wrapp" },
-                        action && action(),
-                        React.createElement(Button, { size: "small", outlined: true, color: "primary", onClick: close }, "Fechar"))))), document.body)
+            React.createElement(Style.ContainerFilter, Object.assign({}, props, { ref: ref }),
+                !notHeader ? (React.createElement(React.Fragment, null,
+                    React.createElement("div", { className: "wrapper-heading" },
+                        icon && React.createElement("span", { className: "icon" }, icon),
+                        React.createElement("div", null,
+                            React.createElement(Text, { element: "h3", bold: true, color: "white" }, title),
+                            React.createElement(Text, { element: "span", size: "p3", color: "white" }, subTitle && subTitle))),
+                    React.createElement("div", { className: "content" }, children))) : (React.createElement("div", { className: "content-not-header" }, children)),
+                React.createElement("div", { className: "button-wrapp" },
+                    action && action(),
+                    React.createElement(Button, { size: "small", outlined: true, color: "primary", onClick: closeModal }, "Fechar")))), document.body)
         : null;
 };
 //# sourceMappingURL=index.js.map
---
+++ dist/utils/animationCloseSideModal.d.ts
@@ -0,0 +1,3 @@
+import { ModalDrawerProps } from '../types/ModalDrawerTypes';
+export declare const animationCloseSideModal: (element: HTMLElement, side: ModalDrawerProps['side']) => Promise<unknown>;
+//# sourceMappingURL=animationCloseSideModal.d.ts.map
