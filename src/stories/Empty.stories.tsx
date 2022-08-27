4b1d27f772c1c10c2841fde0963d3db31eae6f15
Paulo Martin <paulo@lsmenezes.com.br>
2022-08-27 19:02:53 -0300

    feat: add react lottie animation

---
+++ src/stories/Empty.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import Empty, { EmptyProps } from '../components/Empty'
+
+export default {
+  title: 'Empty',
+  component: Empty,
+  args: {
+    title: 'Pagina não encontrada',
+    subTitle: 'Verifique se existem solicitações com filtro especifico',
+    alertType: 'notPage',
+    height: 250,
+    width: 200,
+  },
+} as Meta
+
+export const Default: Story<EmptyProps> = args => (
+  <div style={{ width: '100%', margin: '0 auto' }}>
+    <Empty {...args} />
+  </div>
+)
