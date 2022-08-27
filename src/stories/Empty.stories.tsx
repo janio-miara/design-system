2d46a9c872b3f940857ace9ec340574985628cd1
Paulo Martin <paulo@lsmenezes.com.br>
2022-08-27 19:23:13 -0300

    fix: add to storie Exemple

--- src/stories/Empty.stories.tsx
+++ src/stories/Empty.stories.tsx
@@ -3,7 +3,7 @@ import { Story, Meta } from '@storybook/react'
 import Empty, { EmptyProps } from '../components/Empty'
 
 export default {
-  title: 'Empty',
+  title: 'Exemple/Empty',
   component: Empty,
   args: {
     title: 'Pagina não encontrada',
